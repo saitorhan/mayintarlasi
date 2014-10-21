980c9d8cd2d829960e2a032c3009fb3230119aa0
Sait ORHAN <saitorhan@outlook.com>
2014-10-21 09:21:44 +0300

    Form oluşturuldu

---
+++ MayinTarlasi/MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MayinTarlasi
+{
+    partial class FormMayinTarlasi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTarla = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelTarla
+            // 
+            this.panelTarla.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelTarla.Location = new System.Drawing.Point(0, 57);
+            this.panelTarla.Name = "panelTarla";
+            this.panelTarla.Size = new System.Drawing.Size(584, 504);
+            this.panelTarla.TabIndex = 0;
+            // 
+            // FormMayinTarlasi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.panelTarla);
+            this.Name = "FormMayinTarlasi";
+            this.Text = "Mayın Tarlası";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTarla;
+    }
+}
+
---
+++ MayinTarlasi/MayinTarlasi/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class FormMayinTarlasi : Form
+    {
+        public FormMayinTarlasi()
+        {
+            InitializeComponent();
+        }
+    }
+}
